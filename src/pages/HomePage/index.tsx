66bf9e9bb8f6597c110066236851475c9995b1c5
zeineddinbachtiar <86919853+zeineddinbachtiar@users.noreply.github.com>
2024-11-29 01:34:19 +0700

    Initial commit

---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+interface IHomePageProps {}
+
+const HomePage: React.FunctionComponent<IHomePageProps> = () => {
+  return (
+    <section>
+      <h1 className="text-blue-500">HomePage</h1>
+    </section>
+  );
+};
+
+export default HomePage;
